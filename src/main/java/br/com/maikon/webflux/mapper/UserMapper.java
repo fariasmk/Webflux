cdba073edf6900f2e2c12413baa561dee22f0acc
fariasmk <fariasmk@hotmail.com>
2023-10-06 21:19:42 -0300

    Adiciona import

--- src/main/java/br/com/maikon/webflux/mapper/UserMapper.java
+++ src/main/java/br/com/maikon/webflux/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package br.com.maikon.webflux.mapper;
 
 import br.com.maikon.webflux.entity.User;
 import br.com.maikon.webflux.model.request.UserRequest;
+import br.com.maikon.webflux.model.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
